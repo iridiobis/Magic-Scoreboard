d2f5697b6f6ebb419d60e4c323628401625c8d97
Jose Luis Benito Esteban <iridiobis@gmail.com>
2015-02-08 09:37:59 +0100

    How to create a simple and reusable custom view
    Simple layout with two buttons and a text view, using a custom
    RelativeLayout to provide further functionality.

--- app/src/main/java/es/benitos/magicscoreboard/views/ScoreView.java
+++ app/src/main/java/es/benitos/magicscoreboard/views/ScoreView.java
@@ -1,7 +1,53 @@
 package es.benitos.magicscoreboard.views;
 
-/**
- * Created by iridio on 08/02/15.
- */
-public class ScoreView {
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
+import es.benitos.magicscoreboard.R;
+
+public class ScoreView extends RelativeLayout {
+    @InjectView(R.id.score_score)
+    TextView scoreView;
+
+    public ScoreView(Context context) {
+        super(context);
+    }
+
+    public ScoreView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ScoreView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        ButterKnife.inject(this);
+
+    }
+
+    public void setScore(final int score) {
+        scoreView.setText(Integer.toString(score));
+    }
+
+    @OnClick(R.id.score_add)
+    public void addToScore(){
+        int score = Integer.parseInt(scoreView.getText().toString());
+        score++;
+        setScore(score);
+    }
+
+    @OnClick(R.id.score_subtract)
+    public void subtractToScore(){
+        int score = Integer.parseInt(scoreView.getText().toString());
+        score--;
+        setScore(score);
+    }
 }
